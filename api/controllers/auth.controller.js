eed3fdf2137c4af61b66a66406d4384cb82a8f4d
Giorgi <mrmrela5@gmail.com>
2024-01-22 01:06:37 +0400

    avoid sending of password to client of logged in user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -27,8 +27,9 @@ export const signin = async (req, res, next) => {
     if(!isValidPassword) return next(errorHandler(404, 'Wrong credentials'));
 
     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
-    
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(validUser);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
 
   res.status(201).json({ message: 'User created succesfully' })
   } catch (err) {
