14989ac2bb7bb8b8551b7e0459d0bedd023a1dee
Giorgi <mrmrela5@gmail.com>
2024-01-22 00:43:44 +0400

    complete sign up func - use proxy for /api/user

--- api/index.js
+++ api/index.js
@@ -22,6 +22,6 @@ app.use('/api/auth', authRouter)
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Something went wrong";
-  res.status(statusCode).json({ statusCode, message })
+  res.status(statusCode).json({ success: false, statusCode, message })
   next();
 })
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,29 +1,78 @@
-import Link from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setIsLoading(true);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setErr(data.message);
+        setIsLoading(false);
+        return;
+      }
+
+      setIsLoading(false);
+      setErr(null);
+      navigate('/sign-in');
+    } catch (err) {
+      setErr(err.message);
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="username"
           className="border p-3 rounded-lg"
           id="username"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg upperacase hover:opacity-95 disabled:opacity-80">
-          Sign Up
+        <button
+          disabled={isLoading}
+          className="bg-slate-700 text-white p-3 rounded-lg upperacase hover:opacity-95 disabled:opacity-80"
+        >
+          {isLoading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
 
@@ -33,6 +82,7 @@ const SignUp = () => {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {err && <p className="text-red-500 mt-5">{err}</p>}
     </div>
   );
 };
