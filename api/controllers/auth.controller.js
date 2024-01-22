80cb431a0b896706cc05cd3473cf6926e4d69166
Giorgi <mrmrela5@gmail.com>
2024-01-22 18:26:01 +0400

    check password compare bug & middleware in redux store

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -22,8 +22,8 @@ export const signin = async (req, res, next) => {
   
   try {
     const validUser = await User.findOne({ email });
-    if ( !validUser ) return next(errorHandler(404, 'User not found!'));
-    const isValidPassword = bcryptjs.compare(password, validUser.password);
+    if (!validUser) return next(errorHandler(404, 'User not found!'));
+    const isValidPassword = bcryptjs.compareSync(password, validUser.password);
     if(!isValidPassword) return next(errorHandler(404, 'Wrong credentials'));
 
     const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -5,4 +5,6 @@ export const store = configureStore({
   reducer: {
     user: userReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
 });
